ed71a35f3751ca0e39bb298d5878c91f35de62bd
Xavier <mathewgoodluck46@gmail.com>
2024-03-16 11:16:15 -0400

    Updated Loginpage.tsx
    added the login form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,25 @@
+import {useRef} from "react"
 
-export default function HomePage() {
-    return <h1>Login Here</h1>
+export default function LoginPage() {
+       const usernameRef=useRef(null);        
+        const passwordRef=useRef(null)
+        const handleSubmit=(event: React.FormEvent)=>{
+            event.preventDefault();
+            const username=usernameRef.current?.value;
+            const password=passwordRef.current?.value;
+            if(username && password){
+            alert(`Username: ${username} Password: ${password}`)
+            } else{
+                alert(`Please insert value`)
+            } 
+        }
+    return <>
+    <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" ref={usernameRef}></input>
+        <label htmlFor="password">Password</label>
+        <input type="text" id="password" ref={passwordRef}></input>
+        <button type="submit">Login</button>
+    </form>
+    </>
 }
